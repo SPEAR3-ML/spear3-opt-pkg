c478c27897ae548ffe1e9a4d493baff0cec4db7b
Zhe Zhang <wenatuhs@gmail.com>
2020-09-08 00:11:41 -0700

    Adapt the EGO algorithm to the new Teeport API

--- opt/algorithms/ego.py
+++ opt/algorithms/ego.py
@@ -10,17 +10,20 @@ from ..utils.operators import initialize
 from ..utils.helpers import make_sync
 from operator import itemgetter
 
-async def optimize(evaluate, params):
+async def optimize(evaluate, configs):
     # config
     # D = 8  decision space dimension
     # N0 = 30  initial population
-    # Ng = 200  total generation
     # method = 'lhs'  initialization method
+    # vrange = [0, 1]  initialization variable range
+    # add_center = False  if add the center point of the vrange to init pop
+    # criterion = 'UCB'  SBO|UCB|EI, acquisition to use
+    # T = 300  maximum evaluation number
     evaluate = make_sync(evaluate)
     
     # initialize
     D, N0, method, vrange, add_center, criterion, T = \
-        itemgetter('D', 'N0', 'method', 'vrange', 'add_center', 'criterion', 'T')(params)
+        itemgetter('D', 'N0', 'method', 'vrange', 'add_center', 'criterion', 'T')(configs)
     xdoe = initialize((N0, D), method, vrange, add_center)
     
     xlimits = np.ones((D, 2))
