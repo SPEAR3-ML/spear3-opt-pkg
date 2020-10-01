b65dff6074594ed878924415237486be8e7514e3
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-10-01 00:37:52 -0700

    Add the ZDT test suite

---
+++ opt/evaluators/zdt.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2019-2020, SPEAR3 authors (see AUTHORS.txt).
+# Licensed under the BSD 3-clause license (see LICENSE.txt)
+
+import asyncio
+import numpy as np
+import time
+import pygmo as pg
+
+from operator import itemgetter
+
+async def evaluate(X, configs={
+        'prob_id': 1,
+        'D': 30,
+        'wall_time': 1
+    }):
+    assert type(X) == np.ndarray, 'Input X must be a numpy array'
+
+    prob_id, D, wall_time = itemgetter('prob_id', 'D', 'wall_time')(configs)
+    
+    prob = pg.problem(pg.zdt(prob_id=prob_id, param=D))
+    lower_bound, upper_bound = prob.get_bounds()
+    # denormalize the parameters
+    X = lower_bound + (upper_bound - lower_bound) * X
+    
+    Y = []
+    start_time = time.time()
+    await asyncio.sleep(wall_time)
+    for x in X:
+        y = prob.fitness(x)
+        Y.append(y)
+    end_time = time.time()
+    Y = np.array(Y)
+    print(f'time cost: {(end_time - start_time) * 1e3:.3f} ms')
+    
+    return Y
